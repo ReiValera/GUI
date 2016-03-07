898e1c921911debb0dfd81f0031c195b85448219
Monika Dzhaleva <monicadjaleva@abv.bg>
2016-03-07 17:46:13 +0000

    update

---
+++ WeatherApp/src/weatherapp/sports/PingpongController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherapp.sports;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import weatherapp.IphoneController;
+
+/**
+ * FXML Controller class
+ *
+ * @author gabrieluliano
+ */
+public class PingpongController implements Initializable {
+
+    @FXML
+    Button back;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+
+    public void goback() throws IOException {
+        try {
+            Stage stage = (Stage) back.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("../iPad.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException ex) {
+            Logger.getLogger(IphoneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }     
+    
+}
