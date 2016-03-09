5f754a03b4a92b66664cc43eebd6d8c7aa5b267f
Rei Valera <Owner@161.23.167.97>
2016-03-09 18:28:18 +0000

    api

---
+++ WeatherApp/src/api/TestWeather.java
@@ -0,0 +1,18 @@
+package api;
+public class TestWeather
+{
+
+	public static void main(String[] args)
+	{
+		WeatherAPI weather = new WeatherAPI("44418");
+		System.out.println(weather.theWeatherRSS);
+                System.out.println(weather.Location);
+                System.out.println(weather.CurrentTemp);
+
+		for(int i = 0; i < weather.weatherForecastList.size(); i++)
+		{
+			System.out.println(weather.weatherForecastList.get(i).lowTemp + " " +
+			weather.weatherForecastList.get(i).highTemp);
+		}
+	}
+}
